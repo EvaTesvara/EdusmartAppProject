b50518f93f97f3d676dc9cb3974095ea065ae7b6
Eva Tesvara <eva.tesvara@gmail.com>
2023-10-11 02:30:54 +0700

    jadwal materi ScrollView done

---
+++ app/src/main/java/com/example/edusmart/ActivityMateri1.kt
@@ -0,0 +1,13 @@
+package com.example.edusmart
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityMateri1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_materi1)
+
+        println("hello world")
+    }
+}
