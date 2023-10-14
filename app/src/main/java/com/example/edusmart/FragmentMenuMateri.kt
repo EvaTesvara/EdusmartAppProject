3d3b53ca2f68f589b2b7a7035e80d44b34892176
Eva Tesvara <eva.tesvara@gmail.com>
2023-10-14 18:24:48 +0700

    fragment to fragment change works wonder

--- app/src/main/java/com/example/edusmart/FragmentMenuMateri.kt
+++ app/src/main/java/com/example/edusmart/FragmentMenuMateri.kt
@@ -6,29 +6,56 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.FrameLayout
+import androidx.fragment.app.FragmentTransaction
+import androidx.fragment.app.findFragment
 import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 
 class FragmentMenuMateri : Fragment() {
 
-    private lateinit var btn_seninindo:Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
 
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
+
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_menu_materi, container, false)
+        val view: View = inflater.inflate(R.layout.fragment_menu_materi, container, false)
+
+        val buttonseninindo = view.findViewById<Button>(R.id.seninindo)
+        buttonseninindo.setOnClickListener(View.OnClickListener {
+            val fl:FrameLayout = view.findViewById(R.id.FragmentMenuMateri)
+            fl.removeAllViews()
+            val fragmentBabMateriIndo: Fragment = FragmentBabMateri()
+            val fm: FragmentTransaction? =
+                getActivity()?.getSupportFragmentManager()?.beginTransaction()
+            fm?.replace(R.id.FragmentMenuMateri, fragmentBabMateriIndo)?.commit()
+
+        })
+
+        val buttonsenininggris = view.findViewById<Button>(R.id.senininggris)
+        buttonsenininggris.setOnClickListener(View.OnClickListener {
+            val fl:FrameLayout = view.findViewById(R.id.FragmentMenuMateri)
+            fl.removeAllViews()
+            val fragmentBabMateriInggris: Fragment = FragmentBabMateriInggris()
+            val fm: FragmentTransaction? =
+                getActivity()?.getSupportFragmentManager()?.beginTransaction()
+            fm?.replace(R.id.FragmentMenuMateri, fragmentBabMateriInggris)?.commit()
+
+        })
+
+
+        return view
+
     }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+}
 
 
-        }
 
-    }
+
