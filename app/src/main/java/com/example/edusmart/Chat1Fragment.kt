b482811cdd3fb04146574b16933256e2fd84a205
Eva Tesvara <eva.tesvara@gmail.com>
2023-10-18 20:13:21 +0700

    code merged overlap

--- app/src/main/java/com/example/edusmart/Chat1Fragment.kt
+++ app/src/main/java/com/example/edusmart/Chat1Fragment.kt
@@ -5,10 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.ImageView
-import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.fragment.app.FragmentTransaction
+import com.example.edusmart.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
+ * Use the [Chat1Fragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class FragmentHome : Fragment() {
+class Chat1Fragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -38,19 +35,7 @@ class FragmentHome : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view:View = inflater.inflate(R.layout.fragment_home, container, false)
-
-        val bt = view.findViewById<ImageView>(R.id.img_home1)
-        bt.setOnClickListener{
-            val rl:ConstraintLayout = view.findViewById(R.id.fragment_home)
-            rl.removeAllViews()
-            val toHome1Fragment: Fragment = Home1Fragment()
-            val fragmentChange:FragmentTransaction? = getActivity()?.getSupportFragmentManager()?.beginTransaction()
-            fragmentChange?.replace(R.id.fragment_home, toHome1Fragment)?.commit()
-
-        }
-
-        return view
+        return inflater.inflate(R.layout.fragment_chat1, container, false)
     }
 
     companion object {
@@ -60,12 +45,12 @@ class FragmentHome : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
+         * @return A new instance of fragment Chat1Fragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            FragmentHome().apply {
+            Chat1Fragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
--- app/src/main/java/com/example/edusmart/Home2Fragment.kt
+++ app/src/main/java/com/example/edusmart/Home2Fragment.kt
@@ -16,16 +16,16 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [ChatFragment.newInstance] factory method to
+ * Use the [Home2Fragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class FragmentChat : Fragment() {
+class Home2Fragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
     private var recyclerView : RecyclerView? = null
-    private var recyclerViewChatAdapter : RecyclerViewChatAdapter? = null
-    private var chatList = mutableListOf<Chat>()
+    private var recyclerViewSubScoreAdapter : RecyclerViewSubScoreAdapter? = null
+    private var subscoreList = mutableListOf<SubScore>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,46 +40,47 @@ class FragmentChat : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val v = inflater.inflate(R.layout.fragment_chat, container, false)
 
-        chatList = ArrayList()
+        val v = inflater.inflate(R.layout.fragment_home2, container, false)
 
-        recyclerView = v.findViewById<View>(R.id.rvChatList) as RecyclerView
-        recyclerViewChatAdapter = RecyclerViewChatAdapter(this@FragmentChat, chatList)
+        subscoreList = ArrayList()
+
+        recyclerView = v.findViewById<View>(R.id.rvSubScoreList) as RecyclerView
+        recyclerViewSubScoreAdapter = RecyclerViewSubScoreAdapter(this@Home2Fragment, subscoreList)
         val  layoutManager : RecyclerView.LayoutManager = LinearLayoutManager(context)
         recyclerView!!.layoutManager = layoutManager
-        recyclerView!!.adapter = recyclerViewChatAdapter
+        recyclerView!!.adapter = recyclerViewSubScoreAdapter
 
-        prepareChatListData()
+        prepareScoreListData()
 
         return v
     }
 
-    private fun prepareChatListData(){
-        var chat = Chat("Anne Anindhita, S.Pd", R.drawable.chat_1)
-        chatList.add(chat)
-        chat = Chat("Nita Aryani, S.Pd", R.drawable.chat_2)
-        chatList.add(chat)
-        chat = Chat("Titi Setiawati, S.Pd", R.drawable.chat_3)
-        chatList.add(chat)
-        chat = Chat("Dadi, S.Pd", R.drawable.chat_4)
-        chatList.add(chat)
-        chat = Chat("Elin Kusmayati, S.Pd", R.drawable.chat_5)
-        chatList.add(chat)
-        chat = Chat("Yeni Nuryeni, S.Pd", R.drawable.chat_6)
-        chatList.add(chat)
-        chat = Chat("Risma, S.Pd", R.drawable.chat_7)
-        chatList.add(chat)
-        chat = Chat("Anne Anindhita, S.Pd", R.drawable.chat_1)
-        chatList.add(chat)
-        chat = Chat("Nita Aryani, S.Pd", R.drawable.chat_2)
-        chatList.add(chat)
-        chat = Chat("Titi Setiawati, S.Pd", R.drawable.chat_3)
-        chatList.add(chat)
-        chat = Chat("Dadi, S.Pd", R.drawable.chat_3)
-        chatList.add(chat)
+    private fun prepareScoreListData(){
+        var subscore = SubScore("Tugas 1", R.drawable.home_1)
+        subscoreList.add(subscore)
+        subscore = SubScore("Ulangan Harian 1", R.drawable.home_1)
+        subscoreList.add(subscore)
+        subscore = SubScore("Tugas 2", R.drawable.home_1)
+        subscoreList.add(subscore)
+        subscore = SubScore("UTS", R.drawable.home_1)
+        subscoreList.add(subscore)
+        subscore = SubScore("UAS", R.drawable.home_1)
+        subscoreList.add(subscore)
+        subscore = SubScore("Ujian Praktik", R.drawable.home_1)
+        subscoreList.add(subscore)
+        subscore = SubScore("Kehadiran", R.drawable.home_1)
+        subscoreList.add(subscore)
+        subscore = SubScore("Sikap", R.drawable.home_1)
+        subscoreList.add(subscore)
+        subscore = SubScore("Tambahan", R.drawable.home_1)
+        subscoreList.add(subscore)
+        subscore = SubScore("Konsistensi", R.drawable.home_1)
+        subscoreList.add(subscore)
+        subscore = SubScore("Nilai Akhir", R.drawable.home_1)
+        subscoreList.add(subscore)
 
-        recyclerViewChatAdapter!!.notifyDataSetChanged()
+        recyclerViewSubScoreAdapter!!.notifyDataSetChanged()
 
     }
 
@@ -90,12 +91,12 @@ class FragmentChat : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment ChatFragment.
+         * @return A new instance of fragment Home2Fragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            FragmentChat().apply {
+            Home2Fragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
